a81da985a9c9b56c4749f07ccb49fdc7076fe834
abk377121 <notlvofficial@gmail.com>
2024-12-28 15:26:07 +0530

    control flow 2

---
+++ pickthecloth.java
@@ -0,0 +1,16 @@
+public class pickthecloth {
+    public static void main(String[] args) {
+        boolean sunny = true;
+        boolean hot = false;
+        if (sunny){
+            if(hot){
+                System.out.println("Use half sleeves and a cap");
+            } else {
+                System.out.println("Use half sleeves");
+            }
+            System.out.println("Wear light coloured clothes");
+        } else {
+            System.out.println("Take an umbrella and sweater");
+        }
+    }
+}
