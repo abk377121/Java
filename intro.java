3a50ed9705cbf58f8c2001c49eb760ccea66a76f
abk377121 <notlvofficial@gmail.com>
2025-01-12 15:10:01 +0530

    loops

--- intro.java
+++ intro.java
@@ -1,4 +1,4 @@
-class Main {
+public class intro {
     public static void main(String[] args){
         System.out.println("Hey, Java!");
     }
---
+++ ten10.java
@@ -0,0 +1,7 @@
+public class ten10 {
+    public static void main(String[] args) {
+        for(int i = 10; i >=1; --i){
+            System.out.println("the value of i is: " + i);
+        }
+    }
+}
