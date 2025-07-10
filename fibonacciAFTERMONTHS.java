f15578d99faaf7ce32f6c8aac824fa0c5ce7cc4c
abk377121 <notlvofficial@gmail.com>
2025-07-10 19:54:01 +0530

    fibbionacci series

--- fibonacciAFTERMONTHS.java
+++ fibonacciAFTERMONTHS.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class fibonacciAFTERMONTHS {
-    
+    static int fib(int n){
+        if (n <= 1)
+            return n;
+        return fib(n - 1) + fib(n - 2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Send number of iterations: ");
+        int n = sc.nextInt();
+        System.out.println("Fibonacci series till " + n + ":");
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib(i) + " ");
+        }
+    }
+    //too ez
 }
